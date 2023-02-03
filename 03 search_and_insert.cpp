9fc3d27252dcfbdbb27bcebc20c31756f9814ba2
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2023-02-04 00:31:55 +0530

    Update 03 search_and_insert.cpp

--- 03 search_and_insert.cpp
+++ 03 search_and_insert.cpp
@@ -4,9 +4,8 @@
 
 
 
-
 const int ALPHABET_SIZE = 26; //considering only lowercase english letters
-
+ 
 //an empty trie is represented as => a (root node) where (all 26 children are NULL) AND (isEndOfWord == false)
 
 struct TrieNode 
@@ -22,53 +21,42 @@ struct TrieNode
 }; 
 
 
-struct TrieNode *getNode(void) 
-{ 
-	struct TrieNode *pNode = new TrieNode; 
-
-	pNode->isEndOfWord = false; 
-
-	for (int i = 0; i < ALPHABET_SIZE; i++) 
-		pNode->children[i] = NULL; 
-
-	return pNode; 
-} 
-
-
-void insert(struct TrieNode *root, string key) 
+void insert(struct TrieNode *root, string key) //better use string & key
 { 
 	struct TrieNode *pCrawl = root; 
 
 	for (int i = 0; i < key.length(); i++) 
 	{ 
-		int index = key[i] - 'a'; 
-		if (!pCrawl->children[index]) 
-			pCrawl->children[index] = getNode(); 
+		int index = key[i] - 'a'; 	 //a->0, b->1, c->2...so on...for the children array ka indexing
+		
+		if (pCrawl->children[index] == NULL) 
+			pCrawl->children[index] = new TrieNode(); 
 
 		pCrawl = pCrawl->children[index]; 
 	} 
 
 	 
-	pCrawl->isEndOfWord = true; 
+	pCrawl->isEndOfWord = true;  //last waale node ka (isEndOfWord = true) kar do
 } 
 
 
-bool search(struct TrieNode *root, string key) 
+bool search(struct TrieNode *root, string key)   //better use string & key
 { 
 	struct TrieNode *pCrawl = root; 
 
 	for (int i = 0; i < key.length(); i++) 
 	{ 
-		int index = key[i] - 'a'; 
+		int index = key[i] - 'a';   	//a->0, b->1, c->2...so on...for the children array ka indexing
+		
 		if (!pCrawl->children[index]) 
 			return false; 
 
 		pCrawl = pCrawl->children[index]; 
 	} 
 
-	return (pCrawl != NULL && pCrawl->isEndOfWord); 
+	return (pCrawl != NULL && pCrawl->isEndOfWord); //IMP => 2 conditions
 } 
- 
+
 int main()
 { 
 	
@@ -76,7 +64,7 @@ int main()
 	
 	int n = sizeof(keys)/sizeof(keys[0]); 
 
-	struct TrieNode *root = getNode(); 
+	struct TrieNode *root = new TrieNode(); 
 
 	for (int i = 0; i < n; i++) 
 		insert(root, keys[i]); 
